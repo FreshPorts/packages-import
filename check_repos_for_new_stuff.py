8736caa9c23feff2563ede986cc0a8d90d3e0095
Dan Langille <dan@langille.org>
2020-04-12 14:41:07 +0000

    Update the database with latest repo dates

--- check_repos_for_new_stuff.py
+++ check_repos_for_new_stuff.py
@@ -23,6 +23,9 @@ DSN = 'host=' + config['database']['HOST'] + ' dbname=' + config['database']['DB
 dbh = psycopg2.connect(DSN)
 curs = dbh.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
+# for updating the rows
+cursUpdate = dbh.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
 
 curs.execute("SELECT * from GetReposToReview()")
 NumRows = curs.rowcount
@@ -35,8 +38,15 @@ if (NumRows > 0):
     command = "/usr/home/dan/src/packages-import/get_packagesite.txz_date " + row['abi_name'] + " " + row['branch']
     timestamp = os.popen(command).readlines()
     if timestamp == []:
-      print('nil')
+      date_last_checked = None
     else:
-      print(timestamp[0].strip())
+      date_last_checked = timestamp[0].strip()
 
+    print(date_last_checked)
+      
+    # now we update the last_checked and repo_date in the packages_last_checked table
+    # PackagesLastCheckedSetRepoDate(a_abi_name text, a_branch_name text, a_CheckedDate text)    
+    cursUpdate.callproc('PackagesLastCheckedSetRepoDate', (row['abi_name'],row['branch'], date_last_checked))
+
+dbh.commit();
 dbh.close();
