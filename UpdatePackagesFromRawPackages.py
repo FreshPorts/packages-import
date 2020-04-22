9e3e520b5ed1a3b7fb8722a2f4ae606979b95474
Dan Langille <dan@langille.org>
2020-04-22 23:49:19 +0000

    Update the packages tables from packages_raw
    Invoke this script whenver you want to update the information.

---
+++ UpdatePackagesFromRawPackages.py
@@ -0,0 +1,41 @@
+#!/usr/local/bin/python
+
+import re    # for escaping the database passwords
+#import yaml
+#import io
+#import sys
+import psycopg2
+import psycopg2.extras
+
+#import os
+
+#from psycopg2.extensions import adapt
+
+import configparser # for config.ini parsing
+
+config = configparser.ConfigParser()
+config.read('/usr/local/etc/freshports/config.ini')
+
+SCRIPT_DIR = config['filesystem']['SCRIPT_DIR']
+
+DSN = 'host=' + config['database']['HOST'] + ' dbname=' + config['database']['DBNAME'] + ' user=' + config['database']['PACKAGER_DBUSER'] + ' password=' + re.escape(config['database']['PACKAGER_PASSWORD'])
+
+
+dbh = psycopg2.connect(DSN)
+curs = dbh.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+# for updating the rows
+cursUpdate = dbh.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+
+curs.execute("SELECT * from PackagesGetReposNeedingImports()")
+NumRows = curs.rowcount
+if (NumRows > 0):
+  rows = curs.fetchall()
+  for row in rows:
+    print(row['abi_name'] + '/' + row['package_set']);
+
+    cursUpdate.callproc('UpdatePackagesFromRawPackages', (row['abi_name'],row['package_set']))
+
+cursUpdate.execute("COMMIT");
+dbh.close();
