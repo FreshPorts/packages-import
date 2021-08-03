1fce6d05a5bb600cb56bafe678e111d6fb012e9c
Dan Langille <dan@langille.org>
2021-08-03 16:04:42 +0000

    Add a $ to LOGGERTAG

--- fetch-extract-parse-import-one-abi.sh
+++ fetch-extract-parse-import-one-abi.sh
@@ -17,7 +17,7 @@ mkdir -p $BASEDIR_PACKAGER/$abi/$package_set
 
 if [ ! -d $BASEDIR_PACKAGER ]
 then
-  $LOGGER -t LOGGERTAG "FATAL error: $BASEDIR_PACKAGER does not exist - $0 terminating"
+  $LOGGER -t $LOGGERTAG "FATAL error: $BASEDIR_PACKAGER does not exist - $0 terminating"
   exit 1
 fi
 
@@ -26,7 +26,7 @@ then
   mkdir -p $BASEDIR_PACKAGER/$abi/$package_set
   if [ $? -ne 0 ]
   then
-    $LOGGER -t LOGGERTAG "FATAL error: unable to create $BASEDIR_PACKAGER/$abi/$package_set - $0 terminating"
+    $LOGGER -t $LOGGERTAG "FATAL error: unable to create $BASEDIR_PACKAGER/$abi/$package_set - $0 terminating"
     exit 1
   fi  
 fi
@@ -34,35 +34,35 @@ fi
 cd $BASEDIR_PACKAGER/$abi/$package_set/
 if [ $? -ne 0 ]
 then
-  $LOGGER -t LOGGERTAG "FATAL error: unable to cd $BASEDIR_PACKAGER/$abi/$package_set - $0 terminating"
+  $LOGGER -t $LOGGERTAG "FATAL error: unable to cd $BASEDIR_PACKAGER/$abi/$package_set - $0 terminating"
   exit 1
 fi
 
 rm -f packagesite.txz packagesite.tar
 if [ $? -ne 0 ]
 then
-  $LOGGER -t LOGGERTAG "FATAL error: unable to rm packagesite.txz packagesite.tar - $0 terminating"
+  $LOGGER -t $LOGGERTAG "FATAL error: unable to rm packagesite.txz packagesite.tar - $0 terminating"
   exit 1
 fi  
 
 fetch --quiet https://pkg.freebsd.org/$abi/$package_set/packagesite.txz
 if [ $? -ne 0 ]
 then
-  $LOGGER -t LOGGERTAG "FATAL error: unable to fetch https://pkg.freebsd.org/$abi/$package_set/packagesite.txz - $0 terminating"
+  $LOGGER -t $LOGGERTAG "FATAL error: unable to fetch https://pkg.freebsd.org/$abi/$package_set/packagesite.txz - $0 terminating"
   exit 1
 fi
 
 #unxz packagesite.txz
 #if [ $? -ne 0 ]
 #then
-#  $LOGGER -t LOGGERTAG "FATAL error: unable to unxz packagesite.txz - $0 terminating"
+#  $LOGGER -t $LOGGERTAG "FATAL error: unable to unxz packagesite.txz - $0 terminating"
 #  exit 1
 #fi
 
 tar -xf packagesite.txz
 if [ $? -ne 0 ]
 then
-  $LOGGER -t LOGGERTAG "FATAL error: unable to tar -xf packagesite.txz - $0 terminating"
+  $LOGGER -t $LOGGERTAG "FATAL error: unable to tar -xf packagesite.txz - $0 terminating"
   exit 1
 fi
 
@@ -70,22 +70,22 @@ fi
 $JQ -rc --arg ABI "$abi" --arg PACKAGE_SET "$package_set" '[$ABI, $PACKAGE_SET, .origin, .name, .version] | @tsv' < packagesite.yaml > packagesite.tsv
 if [ $? -ne 0 ]
 then
-  $LOGGER -t LOGGERTAG "FATAL error: unable to run jq to get the tsv file - $0 terminating"
+  $LOGGER -t $LOGGERTAG "FATAL error: unable to run jq to get the tsv file - $0 terminating"
   exit 1
 fi
 
 $SCRIPTDIR/import-via-copy-packagesite-all-raw-fields.py -i packagesite.tsv
 if [ $? -ne 0 ]
 then
-  $LOGGER -t LOGGERTAG "FATAL error: unable to run import-via-copy-packagesite.py to import the file - $0 terminating"
+  $LOGGER -t $LOGGERTAG "FATAL error: unable to run import-via-copy-packagesite.py to import the file - $0 terminating"
   exit 1
 fi
 
-$LOGGER -t LOGGERTAG "finished importing $abi/$package_set"
+$LOGGER -t $LOGGERTAG "finished importing $abi/$package_set"
 
 # this is the return value expected by import_packagesite.py
 echo Done
 
-$LOGGER -t LOGGERTAG "finishes"
+$LOGGER -t $LOGGERTAG "finishes"
 
 exit 0
