8c075a203d21745f92d2d9b8487c7a29c1f2cf43
Dan Langille <dan@langille.org>
2023-09-30 19:30:30 +0000

    Use __file__ instead of the wrong name of this file.

--- import-via-copy-packagesite-all-raw-fields.py
+++ import-via-copy-packagesite-all-raw-fields.py
@@ -28,7 +28,7 @@ def main(argv):
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         print ('import-via-copy-packagesite.py -i <inputfile>')
+         print (__file__  + ' -i <inputfile>')
          sys.exit()
       elif opt in ("-i", "--ifile"):
          inputfile = arg
