1d2840483067c5aa09373850cca1cbccb33ff56a
Dan Langille <dan@langille.org>
2025-02-02 13:25:45 +0000

    Make get_packagesite.txz_date meta.conf aware
    meta.conf tells us what file to fetch for a given repo update
    
    fetch-extract-parse-import-one-abi.sh was meta.conf aware already
    
    see fetch-extract-parse-import-one-abi.sh
    
    What I've done is move the meta.conf parsing into a new file: fetch-parse-meta.conf
    
    fetch-extract-parse-import-one-abi.sh & get_packagesite.txz_date have been amended
    to use it.
    
    Every hour, check_repos_for_new_stuff.py is launched and checks all the repos for
    new stuff. It invokes get_packagesite.txz_date to get the Last-modified date of
    the repo in question. It has been checking the now-deprecate file: packagesite.txz
    
    With pkg 2.x, meta.conf now indicates we need to fetch packagesite.tzst
    
    With this change, check_repos_for_new_stuff.py will now be getting the
    Last-modified date of the correct file and repo processing will be correct.
    
    re: #616 #617

--- fetch-extract-parse-import-one-abi.sh
+++ fetch-extract-parse-import-one-abi.sh
@@ -12,67 +12,17 @@ JQ=/usr/local/bin/jq
 
 $LOGGER -t $LOGGERTAG starting $0
 
-# we just try to create every time
-WORKDIR="$BASEDIR_PACKAGER/$abi/$package_set"
-$LOGGER -t $LOGGERTAG creating $WORKDIR
-mkdir -p $WORKDIR
-
-if [ ! -d $BASEDIR_PACKAGER ]
+# kmod is special on the url
+if [ $package_set == "kmod" ]
 then
-  $LOGGER -t $LOGGERTAG "FATAL error: $BASEDIR_PACKAGER does not exist - $0 terminating"
-  exit 1
-fi
-
-if [ ! -d $WORKDIR ]
-then
-  mkdir -p $WORKDIR
-  if [ $? -ne 0 ]
-  then
-    $LOGGER -t $LOGGERTAG "FATAL error: unable to create $WORKDIR - $0 terminating"
-    exit 1
-  fi  
-fi
-
-$LOGGER -t $LOGGERTAG cd $WORKDIR/
-cd $WORKDIR/
-if [ $? -ne 0 ]
-then
-  $LOGGER -t $LOGGERTAG "FATAL error: unable to cd $WORKDIR - $0 terminating"
-  exit 1
-fi
-
-# NOTE: some older ABI do not have meta.conf
-# Notably
-# * FreeBSD:12:aarch64 latest, 
-# * FreeBSD:12:armv6 latest
-# * FreeBSD:12:armv7
-#
-# If not found, we assumes it is older, and use older default values.
-#
- 
-$LOGGER -t $LOGGERTAG fetch --quiet https://pkg.freebsd.org/$abi/$package_set/meta.conf
-fetch --quiet https://pkg.freebsd.org/$abi/$package_set/meta.conf
-if [ $? -ne 0 ]
-then
-  $LOGGER -t $LOGGERTAG "WARNING: unable to fetch https://pkg.freebsd.org/$abi/$package_set/meta.conf - using default values."
-  ARCHIVE=packagesite
-  ARCHIVE_FILE=packagesite.txz
-  PACKAGE_FILE=packagesite.yaml
+  url_package_set="kmods_latest_2"
 else
-
-  # This is the name of the archive we need to download
-  ARCHIVE=$(grep 'manifests_archive = ' meta.conf | cut -f 2 -d '=' | cut -f 2 -d '"')
-
-  # This is the archive format, usually txz
-  FORMAT=$(grep 'packing_format = ' meta.conf | cut -f 2 -d '=' | cut -f 2 -d '"')
-
-  # This is the name of the file we pull down from the server
-  ARCHIVE_FILE="${ARCHIVE}.${FORMAT}"
-
-  # the name of the file is obtained from meta.conf - look for manifests
-  # This is the archive format, usually txz
-  PACKAGE_FILE=$(grep 'manifests = ' meta.conf | cut -f 2 -d '=' | cut -f 2 -d '"')
+  url_package_set=$package_set
 fi
+
+# we need $package_set to be the right value here...
+# after running this script, we'll have the right value for ARCHIVE_FILE
+. $SCRIPTDIR/fetch-parse-meta.conf
 
 #
 # I'm not sure of the historical reasons for removing these files
@@ -87,12 +37,11 @@ then
   exit 1
 fi  
 
-# grab the file we need
-$LOGGER -t $LOGGERTAG fetch --quiet https://pkg.freebsd.org/$abi/$package_set/${ARCHIVE_FILE}
-fetch --quiet https://pkg.freebsd.org/$abi/$package_set/${ARCHIVE_FILE}
+$LOGGER -t $LOGGERTAG fetch --quiet https://pkg.freebsd.org/$abi/$url_package_set/${ARCHIVE_FILE}
+fetch --quiet https://pkg.freebsd.org/$abi/$url_package_set/${ARCHIVE_FILE}
 if [ $? -ne 0 ]
 then
-  $LOGGER -t $LOGGERTAG "FATAL error: unable to fetch https://pkg.freebsd.org/$abi/$package_set/${ARCHIVE_FILE} - $0 terminating"
+  $LOGGER -t $LOGGERTAG "FATAL error: unable to fetch https://pkg.freebsd.org/$abi/$url_package_set/${ARCHIVE_FILE} - $0 terminating"
   exit 1
 fi
 
